e0f2f1fb2b1ec6b9eac8e124ae37e3fc274b7121
ajsdkty3 <98349232+ajsdkty3@users.noreply.github.com>
2022-03-30 10:44:29 -0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+    if(1==2)
+        {
+            System.out.println("li");
+        }
+
+    System.out.println("hi");
+
+    }
+
+
+
+
+
+}
